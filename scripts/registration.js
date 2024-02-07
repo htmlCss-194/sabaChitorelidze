c60c8d20ed3ee76036ba23b0f65b037d6a1ea773
SabaChitorelidze <chitorelidze.99@gmail.com>
2024-02-07 13:47:40 +0400

    working

--- scripts/registration.js
+++ scripts/registration.js
@@ -9,9 +9,38 @@ function handleLogin() {
 
     
     if (fname.value.length >= 2) {
-        fname.style.borderColor = "green";
-        console.log(fname.value.length)
+        fname.style.borderColor = "rgb(25, 213, 25";
     } else {
         fname.style.borderColor = "red";
     }
+
+    if (lname.value.length >= 2) {
+        lname.style.borderColor = "rgb(25, 213, 25";
+    } else {
+        lname.style.borderColor = "red";
+    }
+
+    if (phoneNumber.value.length === 9) {
+        phoneNumber.style.borderColor = "rgb(25, 213, 25";
+    } else {
+        phoneNumber.style.borderColor = "red";
+    }
+
+    if (personalNum.value.length === 11) {
+        personalNum.style.borderColor = "rgb(25, 213, 25";
+    } else {
+        personalNum.style.borderColor = "red";
+    }
+
+    if (email.value.length >= 2) {
+        email.style.borderColor = "rgb(25, 213, 25";
+    } else {
+        email.style.borderColor = "red";
+    }
+
+    if (password.value.length >= 6) {
+        password.style.borderColor = "rgb(25, 213, 25";
+    } else {
+        password.style.borderColor = "red";
+    };
 }
